41b83b0a14d5a98d397d05bbe57b751aad4f5507
dahi72 <castelli.dahiana@gmail.com>
2025-02-08 14:02:11 -0300

    actividad ciudades

--- src/componentes/AgregarActividad.jsx
+++ src/componentes/AgregarActividad.jsx
@@ -221,7 +221,7 @@ function AgregarActividad() {
                     </FormControl>
                     </Grid>
 
-                    <Grid item xs={12} sm={6}>
+                    {/* <Grid item xs={12} sm={6}>
                         <FormControl fullWidth>
                             <InputLabel>Ciudad</InputLabel>
                             <Select
@@ -255,7 +255,7 @@ function AgregarActividad() {
                             onChange={handleChange}
                             fullWidth
                         />
-                    </Grid>
+                    </Grid> */}
 
                     <Grid item xs={12}>
                         <Button
